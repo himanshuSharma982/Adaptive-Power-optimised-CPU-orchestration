12b2839c01d12e1302fde5a2c2a1f2ff75eb0c74
himanshuSharma982 <lco24379@ccet.ac.in>
2026-04-23 23:59:34 +0530

    Update energy_model.py

--- energy_model.py
+++ energy_model.py
@@ -7,20 +7,20 @@ def compute_power(cpu):
     return 1 + (cpu / 50) ** 3 * 15
 
 def compute_power(cpu):
-    # make high CPU disproportionately expensive
+   
     return 1 + (cpu/100.0)**3 * 12
 def compute_energy(cpu, execution_time, waiting_time):
 
     power = compute_power(cpu)
 
-    # base energy
+
     energy = power * execution_time
 
-    # 🔥 PEAK PENALTY (THIS IS THE KEY)
+   
     if cpu > 60:
         energy += (cpu - 60) ** 2 * execution_time * 0.5
 
-    # small waiting cost
+ 
     energy += waiting_time * 0.05
 
     return energy
